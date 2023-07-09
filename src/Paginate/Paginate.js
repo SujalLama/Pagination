86ac1e46d2ae08b928eab784f602d8ef8d08ab94
Sujal Lama <sujallama@Sujals-MacBook-Air.local>
2023-07-09 17:49:08 +0545

    refactoring

--- src/Paginate/Paginate.js
+++ src/Paginate/Paginate.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 
-const totalPages = 10;
+const totalPages = 6;
 
 export default function Paginate() {
     const [currentPage, setCurrentPage] = useState(1);
@@ -45,79 +45,56 @@ export default function Paginate() {
     */
 
     function showPageNumbers () {
-        let pageNumbers = totalPages > 3 ? Array(3).fill(0) : Array(totalPages).fill(0);;
+        let pageNumbers = totalPages > 3 ? Array(3).fill(0) : Array(totalPages).fill(0);
+
+        const range = createPageRange(pageNumbers);
         
+        /* ================
+            adding dots to the front or back
+        =============*/
+        if(range[pageNumbers.length - 1] < totalPages) {
+            if(range[-1] !== "...") {
+                range.push("...");
+            }
+        }
+
+        return range.map((item) => {
+            return <div key={Date.now() + item} className={`${item == currentPage ? "active" : ""}`}>{item}</div>
+        })
+    }
+
+
+    function createPageRange (pageNumbers) {
         let lowerLimit = 1;
         const higherLimit = totalPages;
 
-        
+        /* ================
+        creating a range [] with three elements
+        =============*/
+
+        for(let i = 0; i < pageNumbers.length; i++ ) {
             // Numbers following first number
             if(currentPage === lowerLimit) {
-        
-                for(let i = 0; i < pageNumbers.length; i++ ) {
-                    pageNumbers[i] = currentPage + i;
-                }
+            pageNumbers[i] = currentPage + i;
 
-                if(totalPages > 3) {
-                    pageNumbers.push("...");
-                }
             }
 
             // Numbers before last number
             if(currentPage === higherLimit) {
-
-                for(let i = 0; i < pageNumbers.length; i++ ) {
-                    let lastIndex = pageNumbers.length - 1;
-                    pageNumbers[lastIndex - i] = currentPage - i;
-                }
-
-                if(totalPages > 3) {
-                    pageNumbers.unshift("...")
-                }
+                let lastIndex = pageNumbers.length - 1;
+                pageNumbers[lastIndex - i] = currentPage - i;
             }
+        }
 
-            // middle page numbers
-            if(currentPage !== lowerLimit && currentPage !== higherLimit) {
+        // middle page numbers
+        if(currentPage !== lowerLimit && currentPage !== higherLimit) {
+            pageNumbers[0] = currentPage - 1;
+            pageNumbers[1] = currentPage;
+            pageNumbers[2] = currentPage + 1;
+        }
+        
 
-                pageNumbers[0] = currentPage - 1;
-                pageNumbers[1] = currentPage;
-                pageNumbers[2] = currentPage + 1;
-
-
-                // if(totalPages > 3) {
-                //     console.log(pageNumbers)
-                //     console.log(currentPage);
-                    
-                //     //  if(pageNumbers[0] === 1) {
-                //     //      pageNumbers.shift("...")
-                //     // }
-                    
-                //     // if(pageNumbers[1] > 2) {
-                //     //     pageNumbers.unshift("...")
-                //     // }
-
-                //     // if(pageNumbers[1] === 2) {
-                //     //     pageNumbers.shift("...")
-                //     // }
-
-    
-    
-                //     // if(pageNumbers[2] < higherLimit) {
-                //     //     pageNumbers.push("...")
-                //     // }
-    
-                //     // if(currentPage === (higherLimit - 1)) {
-                //     //     pageNumbers.pop("...")
-                //     // }
-                // }
-
-            }
-
-
-
-        return pageNumbers.map((item) => {
-            return <div key={item} className={`${item == currentPage ? "active" : ""}`}>{item}</div>
-        })
+        return pageNumbers;  
     }
 
   return (
